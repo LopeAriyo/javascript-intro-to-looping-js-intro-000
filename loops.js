8f0f6b8a13dd4dbbbd9a8d3e803aeeff07ccc1b5
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-13 20:51:55 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+
+  for (var i = 1; i < 25; i++) {
+
+  array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.")
+
+  }
+
+}
+
+var k=0;
+
+function whileLoop(n){
+
+  while (k<=n) {
+    return(n-k)
+    k++
+  }
+  return("done")
+}
+
+var  i=0;
+
+function doWhileLoop(num){
+
+  do {
+    console.log ("I run once regardless.")
+    i++;
+  }
+  while (i<=num);
+
+}
