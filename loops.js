3aed62df23439b697e96c6e94f03ab9141719bfe
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-13 21:23:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,13 @@
 function forLoop(array){
 
-  array.push("I am 1 strange loop.")
+  for (var i = 0; i <= 25; i++) {
 
-  for (var i = 2; i < 25; i++) {
 
-  array.push("I am " + i + " strange loops.")
+    if (i = 0) then {
+      array.push("I am " + i + " strange loop.")
+    } else {
+      array.push("I am " + i + " strange loops.")
+    } 
 
   }
 
